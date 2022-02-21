9b1cf222192880844d9cc87eda074fb1904e1109
Monblo <83069866+Monblo@users.noreply.github.com>
2022-02-21 19:02:23 +0100

    new project structure

--- StoreAngular/API/Entities/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StoreAngular.API.Entities
-{
-    public class Product
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
---
+++ StoreAngular/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.Entities;
+using Infrastructure.Data;
+
+namespace StoreAngular.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = _context.Products.ToList();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return _context.Products.Find(id);
+        }
+    }
+
+}
