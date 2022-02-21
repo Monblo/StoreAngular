e1fb9ca2a62d8e3e700d33691df8397a7a6e35fb
Monblo <83069866+Monblo@users.noreply.github.com>
2022-02-21 15:58:36 +0100

    read data from db

--- StoreAngular/API/Controllers/ProductsController.cs
+++ StoreAngular/API/Controllers/ProductsController.cs
@@ -1,23 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoreAngular.API.Entities;
+using StoreAngular.Data;
 
 namespace StoreAngular.API.Controllers
 {
     [ApiController]
-        [Route("api/[controller]")]
-        public class ProductsController : ControllerBase
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
         {
-
-            [HttpGet]
-            public string GetProducts()
-            {
-                return "this will be a list of products";
-            }
-
-            [HttpGet("{id}")]
-            public string GetProduct(int id)
-            {
-                return "single product";
-            }
+            _context = context;
         }
-    
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = _context.Products.ToList();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return _context.Products.Find(id);
+        }
+    }
+
 }
